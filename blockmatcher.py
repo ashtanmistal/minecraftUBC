4d406479defcdefaf181d8a28a9f39d15a9a927a
Ashtan <ashtanmistal@gmail.com>
2023-05-25 20:29:22 -0700

    Flipped y-axis in data (z axis in Minecraft) to fix orientation issues. Various function renaming to conform to style.

--- blockmatcher.py
+++ blockmatcher.py
@@ -11,6 +11,7 @@ from PIL import Image
 from amulet.api.block import Block
 from amulet_nbt import StringTag
 
+
 # This is a simple Python script that transforms a chunk (16m x 16m) of data and transforms it into a Minecraft chunk.
 # It processes the entire chunk but breaks it down into meter-by-meter blocks.
 
@@ -24,10 +25,11 @@ x_offset = 480000
 y_offset = 5455000
 z_offset = 55
 
+
 allowed_blocks = {  # class 2. Looks like these are roads and stuff
-    # "dirt": Block("minecraft", "dirt"),
+    "dirt": Block("minecraft", "dirt"),
     "stone": Block("minecraft", "stone"),
-    # "moss block": Block("minecraft", "moss_block"),
+    "moss block": Block("minecraft", "moss_block"),
 }
 
 allowed_vegetation = {  # class 3
@@ -75,70 +77,33 @@ allowed_building_blocks = {  # class 6
 # next we need to get the textures for each Minecraft block, and get the average rgb value for each texture
 texture_location = "resources/block"
 
-allowed_blocks_texture = {}
 
-for block, block_object in allowed_blocks.items():
+def get_average_rgb(block_object):
     try:
         texture = Image.open(texture_location + "/" + block_object.base_name + ".png").convert("RGB")
     except FileNotFoundError:
         texture = Image.open(texture_location + "/" + block_object.base_name + "_top.png").convert("RGB")
+        print("top texture used for", block_object.base_name)
 
-    # get the average rgb value for the whole texture
-    allowed_blocks_texture[block] = texture.resize((1, 1), resample=Image.BILINEAR).getpixel((0, 0))
-
-allowed_vegetation_texture = {}
-
-for block, block_object in allowed_vegetation.items():
-    try:
-        texture = Image.open(texture_location + "/" + block_object.base_name + ".png").convert("RGB")
-    except FileNotFoundError:
-        texture = Image.open(texture_location + "/" + block_object.base_name + "_top.png").convert("RGB")
-
-    # get the average rgb value for the whole texture
-    allowed_vegetation_texture[block] = texture.resize((1, 1), resample=Image.BILINEAR).getpixel((0, 0))
-
-allowed_trees_texture = {}
-
-for block, block_object in allowed_trees.items():
-    try:
-        texture = Image.open(texture_location + "/" + block_object.base_name + ".png").convert("RGB")
-    except FileNotFoundError:
-        texture = Image.open(texture_location + "/" + block_object.base_name + "_top.png").convert("RGB")
-
-    # get the average rgb value for the whole texture
-    allowed_trees_texture[block] = texture.resize((1, 1), resample=Image.BILINEAR).getpixel((0, 0))
-
-allowed_building_blocks_texture = {}
-
-for block, block_object in allowed_building_blocks.items():
-    try:
-        texture = Image.open(texture_location + "/" + block_object.base_name + ".png").convert("RGB")
-    except FileNotFoundError:
-        texture = Image.open(texture_location + "/" + block_object.base_name + "_top.png").convert("RGB")
-
-    # get the average rgb value for the whole texture
-    allowed_building_blocks_texture[block] = texture.resize((1, 1), resample=Image.BILINEAR).getpixel((0, 0))
-
-rotation_degrees = 32
-rotation = math.radians(rotation_degrees)
-inverse_rotation_matrix = np.array([[math.cos(rotation), math.sin(rotation), 0],
-                                    [-math.sin(rotation), math.cos(rotation), 0],
-                                    [0, 0, 1]])
-
-print("done computing average rgb values for textures", time.time() - start_time)
-# NOTE that we could do a more complex approach here to get more accurate results, but this is good enough for now
-
-# load the data
-# datasets = []
-# for filename in os.listdir("LiDAR LAS Data/las/"):
-#     if filename.endswith(".las"):
-#         datasets.append(pylas.read("LiDAR LAS Data/las/" + filename))
-# datasets.append(pylas.read("LiDAR LAS Data/las/480000_5457000.las"))
-
-# print("done loading data", time.time() - start_time)
+    return texture.resize((1, 1), resample=Image.BILINEAR).getpixel((0, 0))
 
 
-def transformChunk(data):
+if __name__ == '__main__':
+    allowed_blocks_texture = {block: get_average_rgb(block_object) for block, block_object in allowed_blocks.items()}
+    allowed_vegetation_texture = {block: get_average_rgb(block_object) for block, block_object in
+                                  allowed_vegetation.items()}
+    allowed_trees_texture = {block: get_average_rgb(block_object) for block, block_object in allowed_trees.items()}
+    allowed_building_blocks_texture = {block: get_average_rgb(block_object) for block, block_object in
+                                       allowed_building_blocks.items()}
+    print("done computing average rgb values for textures", time.time() - start_time)
+    rotation_degrees = 30.5
+    rotation = math.radians(rotation_degrees)
+    inverse_rotation_matrix = np.array([[math.cos(rotation), math.sin(rotation), 0],
+                                        [-math.sin(rotation), math.cos(rotation), 0],
+                                        [0, 0, 1]])
+
+
+def transform_chunk(data):
     # We will break it up into 16 1m x 1m "sticks" that span the entire chunk.
     # Whatever data is in that stick tells is what blocks to place in that stick.
     # as there's likely to be more than one block per stick, we want to place all of them.
@@ -167,62 +132,47 @@ def transformChunk(data):
     unique_y, y_indices = np.unique(y, return_index=True)
     unique_z, z_indices = np.unique(z, return_index=True)
 
-    # avg_red = np.zeros((len(unique_x), len(unique_y), len(unique_z)))
-    # avg_green = np.zeros((len(unique_x), len(unique_y), len(unique_z)))
-    # avg_blue = np.zeros((len(unique_x), len(unique_y), len(unique_z)))
-    # avg_class = np.zeros((len(unique_x), len(unique_y), len(unique_z)))
-
     for i, j, k in np.ndindex(len(unique_x), len(unique_y), len(unique_z)):
 
         # For each unique x, y, z coordinate, we want to get the average rgb value for all the data points that have
         # that coordinate. Red, green, and blue are all one-dimensional arrays.
         matching_indices = np.where((x == unique_x[i]) & (y == unique_y[j]) & (z == unique_z[k]))
-        if matching_indices[0].size == 0:
-            continue
+        if matching_indices[0].size == 0 or matching_indices[0].size == 1:
+            continue  # removing sparse data points
         avg_red = np.average(red[matching_indices])
         avg_green = np.average(green[matching_indices])
         avg_blue = np.average(blue[matching_indices])
         avg_class = np.median(labels[matching_indices])  # the most common class in the group
 
         if avg_class == 2:
-            mapped_texture = min(allowed_blocks_texture, key=lambda x: np.linalg.norm(
-                allowed_blocks_texture[x] - np.array([avg_red, avg_green, avg_blue])))
+            mapped_texture = min(allowed_blocks_texture, key=lambda g: np.linalg.norm(
+                allowed_blocks_texture[g] - np.array([avg_red, avg_green, avg_blue])))
             # now we need to get from the name to the actual Block object
-            block = allowed_blocks[mapped_texture]
-            level.set_version_block(unique_x[i].astype(int), unique_z[k].astype(int), unique_y[j].astype(int),
-                                    "minecraft:overworld", game_version, block)
+            selected_block = allowed_blocks[mapped_texture]
         elif avg_class == 3:
-            mapped_texture = min(allowed_vegetation_texture, key=lambda x: np.linalg.norm(
-                allowed_vegetation_texture[x] - np.array([avg_red, avg_green, avg_blue])))
+            mapped_texture = min(allowed_vegetation_texture, key=lambda g: np.linalg.norm(
+                allowed_vegetation_texture[g] - np.array([avg_red, avg_green, avg_blue])))
             # now we need to get from the name to the actual Block object
-            block = allowed_vegetation[mapped_texture]
-            level.set_version_block(unique_x[i].astype(int), unique_z[k].astype(int), unique_y[j].astype(int),
-                                    "minecraft:overworld", game_version, block)
+            selected_block = allowed_vegetation[mapped_texture]
         elif avg_class == 5:
-            mapped_texture = min(allowed_trees_texture, key=lambda x: np.linalg.norm(
-                allowed_trees_texture[x] - np.array([avg_red, avg_green, avg_blue])))
+            mapped_texture = min(allowed_trees_texture, key=lambda g: np.linalg.norm(
+                allowed_trees_texture[g] - np.array([avg_red, avg_green, avg_blue])))
             # now we need to get from the name to the actual Block object
-            block = allowed_trees[mapped_texture]
-            level.set_version_block(unique_x[i].astype(int), unique_z[k].astype(int), unique_y[j].astype(int),
-                                    "minecraft:overworld", game_version, block)
+            selected_block = allowed_trees[mapped_texture]
         elif avg_class == 6:
-            mapped_texture = min(allowed_building_blocks_texture, key=lambda x: np.linalg.norm(
-                allowed_building_blocks_texture[x] - np.array(
+            mapped_texture = min(allowed_building_blocks_texture, key=lambda g: np.linalg.norm(
+                allowed_building_blocks_texture[g] - np.array(
                     [avg_red, avg_green, avg_blue])))
             # now we need to get from the name to the actual Block object
-            block = allowed_building_blocks[mapped_texture]
-            level.set_version_block(unique_x[i].astype(int), unique_z[k].astype(int), unique_y[j].astype(int),
-                                    "minecraft:overworld", game_version, block)
+            selected_block = allowed_building_blocks[mapped_texture]
         else:
-            block = Block("minecraft", "air")
-            level.set_version_block(unique_x[i].astype(int), unique_z[k].astype(int), unique_y[j].astype(int),
-                                    "minecraft:overworld", game_version, block)
-        # print("placed block at", unique_x[i], unique_z[k], unique_y[j], "with rgb", avg_red[i, j, k], avg_green[i, j, k],
-        #       avg_blue[i, j, k], "and texture", mapped_texture)
+            selected_block = Block("minecraft", "air")
+        level.set_version_block(unique_x[i].astype(int), unique_z[k].astype(int), unique_y[j].astype(int),
+                                "minecraft:overworld", game_version, selected_block)
     level.save()
 
 
-def deleteIndices(blue, green, labels, noise_indices, red, x, y, z):
+def delete_indices(blue, green, labels, noise_indices, red, x, y, z):
     x = np.delete(x, noise_indices)
     y = np.delete(y, noise_indices)
     z = np.delete(z, noise_indices)
@@ -233,7 +183,7 @@ def deleteIndices(blue, green, labels, noise_indices, red, x, y, z):
     return blue, green, labels, red, x, y, z
 
 
-def performDatasetTransformation(ds):
+def perform_dataset_transformation(ds):
     x = ds.x
     y = ds.y
     z = ds.z
@@ -243,19 +193,16 @@ def performDatasetTransformation(ds):
     labels = ds.classification
     # denoise: remove all labels that are not 2, 3, 5, or 6
     noise_indices = np.where((labels == 1) | (labels == 9) | (labels == 7) | (labels == 4) | (labels == 8))
-    blue, green, labels, red, x, y, z = deleteIndices(blue, green, labels, noise_indices, red, x, y, z)
+    blue, green, labels, red, x, y, z = delete_indices(blue, green, labels, noise_indices, red, x, y, z)
     # apply the inverse rotation matrix to the x and y coordinates
     x = x - x_offset
     y = y - y_offset
     z = z - z_offset
-    print("performing rotation matrix multiplication")
+
+    print("Done denoising", time.time() - start_time)
     x, y, z = np.matmul(inverse_rotation_matrix, np.array([x, y, z]))
 
     print("done applying rotation matrix", time.time() - start_time)
-
-    # next we need to iterate over these chunks.
-    min_x, min_y, min_z = np.floor(np.min(x)), np.floor(np.min(y)), np.floor(np.min(z))
-    max_x, max_y, max_z = np.ceil(np.max(x)), np.ceil(np.max(y)), np.ceil(np.max(z))
 
     # convert rgb from 0-65535 to 0-255
     red = red / 256
@@ -274,10 +221,17 @@ def performDatasetTransformation(ds):
     print("done sorting data", time.time() - start_time)
 
     # remove data points that are above 256m
+
     indices = np.where(z < 256)
-    x, y, z, red, green, blue, labels = x[indices], y[indices], z[indices], red[indices], green[indices], blue[indices], \
-        labels[
-            indices]
+    x, y, z, red, green, blue, labels = x[indices], y[indices], z[indices], red[indices], green[indices], \
+        blue[indices], labels[indices]
+
+    # the z axis (in Minecraft) was flipped last time, so we need to flip it back
+    y = -y
+
+    # next we need to iterate over these chunks.
+    min_x, min_y, min_z = np.floor(np.min(x)), np.floor(np.min(y)), np.floor(np.min(z))
+    max_x, max_y, max_z = np.ceil(np.max(x)), np.ceil(np.max(y)), np.ceil(np.max(z))
 
     # Next, the script that we will be using to transform the chunk into a Minecraft chunk.
     # Data will be passed in as an array of data points to be treated as LIDAR data.
@@ -297,21 +251,26 @@ def performDatasetTransformation(ds):
             chunk_data = np.array(
                 [x[chunk_indices], y[chunk_indices], z[chunk_indices], red[chunk_indices], green[chunk_indices],
                  blue[chunk_indices], labels[chunk_indices]])
-            transformChunk(chunk_data)
-            print("done with chunk", cx, " ", cy, time.time() - start_time)
-    # print("done transforming chunks", time.time() - start_time)
+            transform_chunk(chunk_data)
+        print("done with chunk range for x =", cx, time.time() - start_time)
+    print("done transforming chunks", time.time() - start_time)
 
 
-# datasets = []
-# level = amulet.load_level("world/UBC")
-# print("done loading level", time.time() - start_time)
+datasets = []
+level = amulet.load_level("world/UBC")
+print("done loading level", time.time() - start_time)
 for filename in os.listdir("LiDAR LAS Data/las/"):
     if filename.endswith(".las"):
-        ds = pylas.read("LiDAR LAS Data/las/" + filename)
-        performDatasetTransformation(ds)
+        dataset = pylas.read("LiDAR LAS Data/las/" + filename)
+        perform_dataset_transformation(dataset)
         print("done transforming chunks for", filename, time.time() - start_time)
-        # break  # for now, just do one dataset
-level.close()
+#         break  # for now, just do one dataset
+
+# dataset = pylas.read("LiDAR LAS Data/las/481000_5456000.las")
+# perform_dataset_transformation(dataset)
+# print("done transforming chunks for", "481000_5456000.las", time.time() - start_time)
+
+
 # for ds in datasets:
 #     level = amulet.load_level("world/UBC")
 #     performDatasetTransformation(ds)
@@ -320,4 +279,4 @@ level.close()
 # level.close() # given this method removes temporary files, it may be a good idea to call this after each dataset is
 # processed, and then reopen it for the next dataset.
 
-print("done", time.time() - start_time)
+# print("done", time.time() - start_time)

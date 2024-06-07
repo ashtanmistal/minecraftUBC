9771b9bb6afad37055de7c423f3b51331daf7488
Ashtan Mistal <ashtanmistal@gmail.com>
2024-06-06 19:51:53 -0700

    Strengthen outlier rejection and decrease block types for visual purposes

--- src/buildings/building_placer.py
+++ src/buildings/building_placer.py
@@ -13,10 +13,8 @@ import numpy as np
 import src.helpers
 
 BUILDING_BLOCKS = {
-    "bricks": Block("minecraft", "bricks"),
     "stone bricks": Block("minecraft", "stone_bricks"),
     "iron block": Block("minecraft", "iron_block"),
-    "deepslate tiles": Block("minecraft", "deepslate_tiles"),
 }
 BUILDING_BLOCKS_TEXTURES = {
     block: src.helpers.get_average_rgb(block_object) for block, block_object in BUILDING_BLOCKS.items()
@@ -29,7 +27,7 @@ OBJ_DIRECTORY = (r"C:\Users\Ashtan Mistal\OneDrive - "
 OUTLIER_THRESHOLD = 100
 OUTLIER_RADIUS = 1.4
 TRUNCATION_RADIUS = 0.2
-TRUNCATION_THRESHOLD = 0.5
+TRUNCATION_THRESHOLD = 0.6
 HEIGHT_OFFSET = 59
 
 
@@ -57,7 +55,28 @@ def process_obj(obj_file, level):
             y = -y
             r, g, b = map(int, parts[4:7])
             points[i] = [x, y, z, r, g, b]
+    process_points(points, level, os.path.basename(obj_file))
 
+
+def process_las(las_file, level):
+    """
+    Reads an LAS file and performs the same processing as process_obj
+    :param las_file:
+    :param level:
+    :return:
+    """
+    pass  # TODO implement this
+
+
+def process_points(points, level, basename):
+    """
+    This function processes the points in the point cloud to place the buildings in the level.
+    :param points: Nx6 array of points
+    :param level: Amulet level object
+    :param basename: The name of the file being processed (for logging purposes)
+    :return: None
+    """
+    size = len(points)
     tree = cKDTree(points[:, :2])
 
     # rejecting outlier points: for each point, find the nearest neighbours within a radius of 1.5 meters
@@ -70,7 +89,10 @@ def process_obj(obj_file, level):
         if len(neighbours) < OUTLIER_THRESHOLD:
             rejects[i] = True
 
-    print(f"Rejecting {np.sum(rejects)} points as outliers out of {size} points")
+    # print(f"Rejecting {np.sum(rejects)} points as outliers out of {size} points")
+    if np.sum(rejects) == size:
+        print(f"Warning: All points in {basename} were rejected during outlier detection")
+        return
 
     points = points[~rejects]
     size = len(points)
@@ -89,11 +111,13 @@ def process_obj(obj_file, level):
         if np.sum(np.all(neighbour_points_rounded == rounded_point, axis=1)) < len(neighbours) * TRUNCATION_THRESHOLD:
             rejects[i] = True
 
-    # print(f"Rejecting {np.sum(rejects)} points due to truncation errors out of {size} points")
+    print(f"Rejecting {np.sum(rejects)} points due to truncation errors out of {size} points")
     points = points[~rejects]
     size = len(points)
 
-
+    if size == 0:
+        print(f"Warning: All points in {basename} were rejected during truncation error check")
+        return
 
     # placing the buildings in the level
     # switch the Y and Z coordinates to match the Minecraft coordinate system
@@ -105,11 +129,13 @@ def process_obj(obj_file, level):
     min_x, max_x = np.floor(np.min(points_x) / 16) * 16, np.ceil(np.max(points_x) / 16) * 16
     min_z, max_z = np.floor(np.min(points_z) / 16) * 16, np.ceil(np.max(points_z) / 16) * 16
 
+    rejected = 0
+
     for chunk_block_x in range(min_x.astype(int), max_x.astype(int), 16):
         for chunk_block_z in range(min_z.astype(int), max_z.astype(int), 16):
             points_in_chunk = np.where(
                 (points_x >= chunk_block_x) & (points_x < chunk_block_x + 16) & (points_z >= chunk_block_z) & (
-                            points_z < chunk_block_z + 16))
+                        points_z < chunk_block_z + 16))
             if len(points_in_chunk[0]) == 0:
                 continue
             chunk_data = np.array([points_x[points_in_chunk], points_y[points_in_chunk], points_z[points_in_chunk],
@@ -131,13 +157,41 @@ def process_obj(obj_file, level):
             for x, y, z in np.ndindex(16, max_y - min_y + 1, 16):
                 if chunk_color_count[x, y, z] == 0:
                     continue
+                # check neighbouring blocks in the xz plane for stray blocks
+                if x > 0:  # no need to check y bounds; all points are above ground + height offset is not yet applied
+                    if z > 0:
+                        if np.sum(chunk_color_count[x - 1:x + 2, y - 1:y + 2, z - 1:z + 2] > 0) < 4:
+                            rejected += 1
+                            continue
+                    else:
+
+                        if np.sum(chunk_color_count[x - 1:x + 2, y - 1:y + 2, z:z + 2] > 0) < 2:
+                            rejected += 1
+                            continue
+                else:
+                    if z > 0:
+                        if np.sum(chunk_color_count[x:x + 2, y - 1:y + 2, z - 1:z + 2] > 0) < 2:
+                            rejected += 1
+                            continue
+                    else:
+                        # along chunk boundary. can't reject
+                        pass
                 average_color = chunk_color_matrix[x, y, z] / chunk_color_count[x, y, z]
-                mapped_color = min(BUILDING_BLOCKS, key=lambda b: np.linalg.norm(BUILDING_BLOCKS_TEXTURES[b] - average_color))
+                mapped_color = min(BUILDING_BLOCKS,
+                                   key=lambda b: np.linalg.norm(BUILDING_BLOCKS_TEXTURES[b] - average_color))
                 mapped_block = BUILDING_BLOCKS[mapped_color]
-                universal_block, universal_block_entity, universal_extra = level.translation_manager.get_version("java", (1, 20, 4)).block.to_universal(mapped_block)
+                universal_block, universal_block_entity, universal_extra = level.translation_manager.get_version("java",
+                                                                                                                 (1, 20,
+                                                                                                                  4)).block.to_universal(
+                    mapped_block)
                 block_id = level.block_palette.get_add_block(universal_block)
                 chunk.blocks[x, y + height_offset, z] = block_id
             chunk.changed = True
+
+    print(f"Rejected {rejected} blocks due to insufficient neighbours out of {size} blocks")
+    if rejected / size > 0.01:
+        print(f"Warning: Neighbour algorithm rejected more than 1% of the blocks in {basename}")
+
 
 def main():
     # Minecraft world copying: copy the entire world/STREETLIGHTS directory to world/BUILDINGS
@@ -153,19 +207,24 @@ def main():
 
     files_to_process = []
 
+    # for filename in os.listdir(OBJ_DIRECTORY):
     for filename in os.listdir(OBJ_DIRECTORY):
         if filename.endswith("_pred.obj"):
             obj_file = os.path.join(OBJ_DIRECTORY, filename)
             files_to_process.append(obj_file)
-    level = amulet.load_level("../world/BUILDINGS")
+
     for filename in tqdm(files_to_process):
+        level = amulet.load_level("../world/BUILDINGS")
         process_obj(filename, level)
         # import pdb; pdb.set_trace()
-    level.save()
-    level.close()
+        level.save()
+        level.close()
 
-    print("Buildings placed in world/BUILDINGS")
+    print("Buffer Buildings placed in world/BUILDINGS. Processing remaining buildings...")
     # level.close()
+
+    # TODO get buildings that do are not derived from LiDAR-DenseSeg buffer and place them in the world naively
+    # This includes all building points in the set {ubcv_legal_boundary - buffer} U {ubcv_uel}
 
 
 if __name__ == "__main__":
